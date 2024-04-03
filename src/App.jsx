5e22c8faca5c543a1bf6e8205e56f725af58f6c2
Luis Gil <luisfrancisco17msn@gmail.com>
2024-04-03 17:59:24 -0500

    Modificación de componentes formulario y lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import TaskForm from './components/TaskForm'
 import TasksList from './components/TasksList'
 
 const App = () => {
 
-  const tasksState = useSelector(state => state.tasks)
-  console.log(tasksState)
-  
   return (
     <div className='App'>
       <h1>CRUD</h1>
--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
 
 const TasksList = () => {
+
+    const tasks = useSelector(state => state.tasks)
+    console.log(tasks)
+
   return (
-    <div>TasksList</div>
+    <>
+        {
+            tasks.map( task => (
+                <div key={task.id}>
+                    <h3>{task.title}</h3>
+                    <p>{task.description}</p>
+                </div>
+            ))
+        }
+    </>
   )
 }
 
