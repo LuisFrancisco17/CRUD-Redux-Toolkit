cda538ce4ebffbd36bf88392542fc6ec81c43f47
Luis Gil <luisfrancisco17msn@gmail.com>
2024-04-04 11:32:46 -0500

    Se agrego estilos mediante tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 const App = () => {
 
   return (
-    <div className='App'>
-      <h1>CRUD</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<TasksList />} />
-          <Route path='/create-task' element={<TaskForm />}/>
-          <Route path='/edit-task/:id' element={<TaskForm />}/>
-        </Routes>
-      </BrowserRouter>
+    <div className='bg-zinc-900 h-screen text-white'>
+      <div className='flex items-center justify-center h-full'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<TasksList />} />
+            <Route path='/create-task' element={<TaskForm />}/>
+            <Route path='/edit-task/:id' element={<TaskForm />}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   )
 }
