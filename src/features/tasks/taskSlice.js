85e52e1b0cc438b838c712b1748eba8ac4492be7
Luis Gil <luisfrancisco17msn@gmail.com>
2024-04-03 20:26:38 -0500

    Creación de método addTask

--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -18,8 +18,12 @@ export const taskSlice = createSlice({
     name: 'tasks',
     initialState,
     reducers: {
-
+        addTask: (state, action) => {
+            state.push(action.payload)
+        }
     }
 })
+
+export const {addTask} = taskSlice.actions
 
 export default taskSlice.reducer
